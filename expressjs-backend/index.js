2fe3494bd3856d93b7c6ffc3c7c1b3d80a943d98
annettetrann <annettetran5@gmail.com>
2024-04-22 15:18:41 -0700

    added static data in new GET route

--- expressjs-backend/index.js
+++ expressjs-backend/index.js
@@ -7,8 +7,46 @@ app.use(express.json());
 //get(first parameter is the path, second parameter is the callback function)
 app.get('/', (req, res) => {
     res.send('Hello World!');
-});
+    }
+);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});
+    }
+);
+
+const users = {
+    users_list:
+        [
+            {
+                id: 'xyz789',
+                name: 'Charlie',
+                job: 'Janitor',
+
+            },
+            {
+                id: 'abc123',
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                id: 'ppp222',
+                name: 'Mac',
+                job: 'Professor',
+            },
+            {
+                id: 'yat999',
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                id: 'zap555',
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ]
+}
+app.get('/users', (req, res) => {
+    res.json(users);
+    }
+);
