cccc21f5e864be95d78f873f346ca330bff19af2
annettetrann <annettetran5@gmail.com>
2024-04-30 22:39:45 -0700

    finally fixed

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,6 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MyApp from './MyApp'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import MyApp from './MyApp'
+// import './index.css'
+// import './global.css'
+// import Homepage from './pages/HomePage'
 
-ReactDOM.render(<MyApp />, document.getElementById('root'))
+// // ReactDOM.render(<MyApp />, document.getElementById('root'))
+// ReactDOM.render(<Homepage/>, document.getElementById('root'))
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import Router from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </React.StrictMode>
+);
+
