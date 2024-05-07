46772d38d947fac5f6ff7eb8c2ba87742424aac9
annettetrann <annettetran5@gmail.com>
2024-05-07 09:14:56 -0700

    added beatmap listing

---
+++ react-frontend/src/pages/BeatmapPage/BeatmapListingPage.js
@@ -0,0 +1,15 @@
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./BeatmapListingPage.module.css";
+
+import React from "react";
+
+const BeatmapListingPage = () => { 
+    return (
+        <div>
+            <h1>Beatmap Listing Page</h1>
+        </div>
+    );
+};
+
+export default BeatmapListingPage;
