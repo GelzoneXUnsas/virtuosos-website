9ce8b25c31496cd686ebfec7a881793a206e5c1c
annettetrann <annettetran5@gmail.com>
2024-05-14 23:00:39 -0700

    navbar v1 static no routes

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './Header.module.css';
 
@@ -7,15 +7,26 @@ import logoIcon from '../../assets/icons/logoIcon.svg';
 import closedMenuIcon from '../../assets/icons/closedMenuIcon.svg';
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <div className={styles.header} >
             <div className={styles.headerLogo}>
                 <img className={styles.headerLogoImage} alt="" src={ logoIcon } />
             </div>
             <div className={styles.headerMenu}>
-                <div className={styles.headerMenuButton}>
+                <div className={styles.headerMenuButton} onClick={()=>{setOpen(!open)}}>
                     <b className={styles.headerMenuButtonText}>MENU</b>
                     <img className={styles.menubuttonIcon} alt="" src={closedMenuIcon } />
+                </div>
+                
+                <div className={`${styles.dropdownMenu} ${open? "active" : ""}`}>
+                    <ul>
+                        <DropdownItem text={"LOGIN"} link="/" />
+                        <DropdownItem text={"MUSIC"} link="/" />
+                        <DropdownItem text={"ART"} link="/gallery" />
+                        <DropdownItem text={"COMMUNITY"} link="/beatmaplisting" />
+                    </ul>
                 </div>
             </div>
             
@@ -23,5 +34,15 @@ const Header = () => {
         </div>
     );
 }
+
+function DropdownItem(props) {
+    return(
+        <li className='dropdownItem'>
+            <a>{props.text}</a>
+        </li>
+
+    );
+}
+
 
 export default Header;
