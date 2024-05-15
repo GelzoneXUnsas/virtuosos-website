8ed88c630157207699edfe4cd59e3088e6324c80
annettetrann <annettetran5@gmail.com>
2024-05-15 00:09:44 -0700

    added some routing

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-
+import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 
@@ -9,12 +9,28 @@ import closedMenuIcon from '../../assets/icons/closedMenuIcon.svg';
 const Header = () => {
     const [open, setOpen] = useState(false);
 
+    let menuRef = useRef();
+
+    useEffect(() => {
+        let handler = (e)=>{
+            if(!menuRef.current.contains(e.target)){
+                setOpen(false);
+                console.log(menuRef.current);
+            }
+        };
+        document.addEventListener("mousedown", handler);
+
+        return ()=>{
+            document.removeEventListener("mousedown", handler);
+        }
+    });
+
     return (
         <div className={styles.header} >
             <div className={styles.headerLogo}>
                 <img className={styles.headerLogoImage} alt="" src={ logoIcon } />
             </div>
-            <div className={styles.headerMenu}>
+            <div className={styles.headerMenu} ref={menuRef}>
                 <div className={styles.headerMenuButton} onClick={()=>{setOpen(!open)}}>
                     <b className={styles.headerMenuButtonText}>MENU</b>
                     <img className={styles.menubuttonIcon} alt="" src={closedMenuIcon } />
@@ -22,10 +38,10 @@ const Header = () => {
                 
                 <div className={`${styles.dropdownMenu} ${open? "active" : ""}`}>
                     <ul>
-                        <DropdownItem text={"LOGIN"} link="/" />
-                        <DropdownItem text={"MUSIC"} link="/" />
-                        <DropdownItem text={"ART"} link="/gallery" />
-                        <DropdownItem text={"COMMUNITY"} link="/beatmaplisting" />
+                        <DropdownItem text={"Login"} link="/" />
+                        <DropdownItem text={"Music"} link="/beatmaplisting" />
+                        <DropdownItem text={"Art"} link="/gallery" />
+                        <DropdownItem text={"Community"} link="/" />
                     </ul>
                 </div>
             </div>
@@ -36,10 +52,19 @@ const Header = () => {
 }
 
 function DropdownItem(props) {
+    const navigate = useNavigate();
+    
+    function handleClick(){
+        navigate(props.link);
+    }
+
     return(
-        <li className='dropdownItem'>
-            <a>{props.text}</a>
-        </li>
+        <button type="button" className={styles.dropdownItem} onClick={handleClick}>
+            {props.text}
+        </button>
+        // <li className='dropdownItem'>
+        //     <a>{props.text}</a>
+        // </li>
 
     );
 }
